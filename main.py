a144717a202758b0db65af69ad83f0e708c01c32
Abdullah Haydar <Abdullah.Haydar@erpmaestro.com>
2018-04-17 10:31:58 +0200

    Allow user to send api key

--- main.py
+++ main.py
@@ -20,23 +20,20 @@ class Goelocation(Resource):
     def get(self):
         return request.data # Fetches first
 
-    def post(self):
+    def post(self,key):
+        
         arr = request.json
-        n = 0
         a = []
         for i in arr:
             b = {"wifiAccessPoints":[]}
             for j in i['apscan_data']:
                 b['wifiAccessPoints'].append({"macAddress":j['bssid']})
-            url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAn5aF2aAGpTOZh3ZZT86vcfa-PSUIoxoI"
+            url = "https://www.googleapis.com/geolocation/v1/geolocate?key="+key
             r = cached_sess.post(url,json=b)
             a.append(r.text)
-            n = n +1
-            if(n == 2):
-                break
         return jsonify(a)
 
-api.add_resource(Goelocation, '/goelocation')
+api.add_resource(Goelocation, '/goelocation/<key>')
 
 if __name__ == "__main__":
     app.run(debug=True,port=5002)
